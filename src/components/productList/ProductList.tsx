4e969a54c1f464fbaaaa5492689e85d2aa21abae
T0nete <tonbarramon@gmail.com>
2023-05-18 23:49:08 +0200

    product list working with filters

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -1,11 +1,18 @@
+import { useAppSelector } from '../../hooks/store'
 import { productListMock } from '../../mock/productListMock'
 import ProductCard from './ProductCard'
 
 const ProductList: React.FC = () => {
+  const categorySelected = useAppSelector(state => state.categories)
+
+  const productsList = categorySelected === -1
+    ? productListMock
+    : productListMock.filter((product) => product.categoryId === categorySelected)
+
   return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
             {
-                productListMock.map((product) => {
+                productsList.map((product) => {
                   return (
                         <ProductCard key={product.id} product={product} />
                   )
--- src/mock/productListMock.ts
+++ src/mock/productListMock.ts
@@ -14,64 +14,64 @@ import trousers3 from '../assets/img/jacket3_3.jpg'
 import trousers4 from '../assets/img/jacket3_4.jpg'
 import trousers5 from '../assets/img/jacket3_5.jpg'
 
-import { Product } from '../types/productTypes'
+import { type Product } from '../types/productTypes'
 
 export const productMock: Product = {
-    id: 1,
-    name: 'Jacket',
-    imgs: [
-        jacket1,
-        jacket2,
-        jacket3,
-        jacket4,
-        jacket5,
-    ],
-    price: 100,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
-    category: 'Clothes',
+  id: 1,
+  name: 'Jacket',
+  imgs: [
+    jacket1,
+    jacket2,
+    jacket3,
+    jacket4,
+    jacket5
+  ],
+  price: 100,
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
+  categoryId: 5
 }
 
 export const productListMock: Product[] = [
-    {
-        id: 1,
-        name: 'Jacket',
-        imgs: [
-            jacket1,
-            jacket2,
-            jacket3,
-            jacket4,
-            jacket5,
-        ],
-        price: 100,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
-        category: 'Clothes'
-    },
-    {
-        id: 2,
-        name: 'Shirt',
-        imgs: [
-            shirt1,
-            shirt2,
-            shirt3,
-            shirt4,
-            shirt5,
-        ],
-        price: 50,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
-        category: 'Clothes'
-    },
-    {
-        id: 3,
-        name: 'Trousers',
-        imgs: [
-            trousers1,
-            trousers2,
-            trousers3,
-            trousers4,
-            trousers5,
-        ],
-        price: 70,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
-        category: 'Clothes'
-    }
+  {
+    id: 1,
+    name: 'Jacket',
+    imgs: [
+      jacket1,
+      jacket2,
+      jacket3,
+      jacket4,
+      jacket5
+    ],
+    price: 100,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
+    categoryId: 5
+  },
+  {
+    id: 2,
+    name: 'Shirt',
+    imgs: [
+      shirt1,
+      shirt2,
+      shirt3,
+      shirt4,
+      shirt5
+    ],
+    price: 50,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
+    categoryId: 1
+  },
+  {
+    id: 3,
+    name: 'Trousers',
+    imgs: [
+      trousers1,
+      trousers2,
+      trousers3,
+      trousers4,
+      trousers5
+    ],
+    price: 70,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam sapien aliquet nunc, nec ultricies nisl nunc quis nunc.',
+    categoryId: 6
+  }
 ]
