ddcbff9d34e7871252e9922898a201de7089a1ee
T0nete <tonbarramon@gmail.com>
2023-05-22 22:59:05 +0200

    img view

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -10,44 +10,57 @@ const ProductDetail: React.FC = () => {
   const [selectedImg, setSelectedImg] = React.useState<string>('')
   const [imgsList, setImgsList] = React.useState<string[]>([])
 
-
   useEffect(() => {
     getProduct(Number(id))
-  }, [id])
+  }, [])
 
   useEffect(() => {
     if (product !== null) {
       setSelectedImg(product.imgs[0])
-      const imgsList = product.imgs.filter((img) => img !== selectedImg)
+      const imgsList = product.imgs.filter((img) => img !== product.imgs[0])
       setImgsList(imgsList)
     }
   }, [product])
 
+  const handleImgClick = (image: string) => {
+    if (product === null) return
+    const imgsList = product?.imgs.filter((img) => img !== image)
+    setImgsList(imgsList)
+    setSelectedImg(image)
+  }
+
   return (
-    <div className='mx-auto w-4/5 bg-slate-400'>
+    <div className='mx-auto w-10/12'>
         {loading && <p>Loading...</p>}
         {(product !== null) && (
-            <div className='flex flex-row'>
-                <div className='flex flex-col w-5/12'>
-                    <img src={selectedImg} alt={product.name} className='rounded-xl' />
-                    <div className='flex flex-row'>
-                      {
-                        imgsList.map((img) => {
-                          return (
-                            <div key={img} className='h-32'>
-                              <img src={img} alt={product.name} className='rounded-xl' />
-                            </div>
-                          )
-                        })
-                      }
-                    </div>
-                </div>
-                <div className='flex flex-col w-7/12'>
-                    <h1>{product.name}</h1>
-                    <p>{product.description}</p>
-                    <p>{product.price}</p>
+          <div className='flex flex-row p-2'>
+            <div className='flex flex-col w-5/12 '>
+                <img
+                  src={selectedImg}
+                  alt={product.name}
+                  className='rounded-xl object-contain shadow-lg'
+                />
+                <div className='flex flex-row justify-evenly p-2'>
+                  {
+                    imgsList.map((img) => {
+                      return (
+                        <img
+                          key={img}
+                          src={img}
+                          alt={product.name}
+                          onClick ={() => { handleImgClick(img) }}
+                          className='shadow-xl h-28 rounded-lg object-contain cursor-pointer' />
+                      )
+                    })
+                  }
                 </div>
             </div>
+            <div className='flex flex-col w-7/12'>
+                <h1>{product.name}</h1>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+            </div>
+          </div>
         )}
     </div>
   )
